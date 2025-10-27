8ba1cf1db1ac5b77e6ab11491aee6ad9ba209c01
Shannon Donnelly <shannon.donnelly@sap.com>
2025-10-27 15:56:29 +0000

    added a DBconn (something new)

--- src/DBConnection.java
+++ src/DBConnection.java
@@ -4,10 +4,11 @@ import java.sql.SQLException;
 
 public class DBConnection {
     // Database connection details
+    //added this so i can copy it to other projects easily
      public static Connection getConnection() throws SQLException {
         String url = "jdbc:mysql://localhost:3306/gaming_portal?serverTimezone=UTC";
         String user = "root";
-        String password = "root"; // Change if needed
+        String password = "root"; 
         return DriverManager.getConnection(url, user, password);
     }
 }
