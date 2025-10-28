e2206988dbd15416c64d275494aae98eadc06ff0
Shannon Donnelly <shannon.donnelly@sap.com>
2025-10-28 16:18:56 +0000

    login page

--- src/LoginPage.java
+++ src/LoginPage.java
@@ -8,5 +8,44 @@ public class LoginPage extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) 
         throws ServletException, IOException {
 
+            response.setContentType("text/html");
+            PrintWriter out = response.getWriter();
+
+            String gamerTag = request.getParameter("gamerTag");
+            String password = request.getParameter("password");
+
+            try {
+            Connection conn = DBConnection.getConnection();
+            PreparedStatement ps = conn.prepareStatement(
+                "SELECT credits FROM users WHERE gamerTag=? AND password=?");
+            ps.setString(1, gamerTag);
+            ps.setString(2, password);
+            ResultSet rs = ps.executeQuery();
+
+            out.println("<html><body>");
+
+            if (rs.next()) {
+                int credits = rs.getInt("credits");
+                out.println("<h3>Welcome " + gamerTag + "!</h3>");
+                out.println("<p>Your current game credits: " + credits + "</p>");
+                out.println("<form action='credits' method='post'>");
+                out.println("Add/Spend Credits: <input type='number' name='change' />");
+                out.println("<input type='hidden' name='gamerTag' value='" + gamerTag + "' />");
+                out.println("<input type='submit' value='Update Credits' />");
+                out.println("</form>");
+            } else {
+                out.println("<h3>Invalid login. Please try again.</h3>");
+                out.println("<a href='login.html'>Go Back</a>");
+            }
+
+            out.println("</body></html>");
+
+            rs.close();
+            ps.close();
+            conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            out.println("<html><body>");
         }
- }
+    }
+}   
